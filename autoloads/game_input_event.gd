04cd7d971b9da3b6398e0554593ad6fc93f53302
leahskywalker <leahg726@gmail.com>
2025-02-01 17:04:10 -0700

    Adding Autoloads Folder
    Adding autoloads folder with game_input_event.gd script.

---
+++ autoloads/game_input_event.gd
@@ -0,0 +1,16 @@
+#GameInputEvent.gd
+extends Node
+
+static func movement_input() -> Vector2:
+	var direction = Vector2.ZERO
+	
+	if Input.is_action_pressed("walk_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("walk_right"):
+		direction.x += 1
+	if Input.is_action_pressed("walk_up"):
+		direction.y -= 1
+	if Input.is_action_pressed("walk_down"):
+		direction.y += 1
+	
+	return direction.normalized()
