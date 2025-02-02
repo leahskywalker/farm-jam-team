12d181c30fff3c10eaa07556d3a914879d252d9f
leahskywalker <leahg726@gmail.com>
2025-02-01 19:47:30 -0700

    Adding World Scene and Dialog Area
    Adding world object scene and dialog area scene and script.

---
+++ objects/world/dialog_area.gd
@@ -0,0 +1,17 @@
+#DialogArea.gd
+extends Area2D
+
+@export var text_key: String = ""
+var area_active: bool = false
+
+func _input(event):
+	if area_active and event.is_action_pressed("interact"):
+		SignalBus.emit_signal("display_dialog", text_key)
+
+
+func _on_area_entered(area: Area2D) -> void:
+	area_active = true
+
+
+func _on_area_exited(area: Area2D) -> void:
+	area_active = false
