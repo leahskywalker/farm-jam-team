605670c5f9824b9e9bb0d50311697af4826e3dd2
Galesy <heitor@estevam.org>
2025-02-16 11:55:14 -0300

    fixed some gitignore stuff

--- objects/characters/player/player.gd
+++ objects/characters/player/player.gd
@@ -2,15 +2,25 @@
 class_name Player
 extends CharacterBody2D
 
+@onready var animplayer = $CharacterSETUP/body/AnimationPlayer
+@onready var spritenode = $CharacterSETUP
+
 @export var speed: float = 200
 @export var inv: Inv
 
 func _physics_process(_delta: float) -> void:
 	var direction = GameInputEvent.movement_input()
 	
+	if direction.x < 0:
+		spritenode.scale.x = -1
+	elif direction > 0:
+		spritenode.scale.x = 1
+	
 	if direction != Vector2.ZERO:
+		animplayer.play("runPLAYER")
 		velocity = direction * speed
 	else:
+		animplayer.play("idlePLAYER")
 		velocity = Vector2.ZERO
 	
 	move_and_slide()
