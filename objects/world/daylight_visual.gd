ef16c835efaa0055d6798387884c64a24b71f946
Galesy <heitor@estevam.org>
2025-02-28 16:07:55 -0300

    edit daylight math

--- objects/world/daylight_visual.gd
+++ objects/world/daylight_visual.gd
@@ -3,4 +3,4 @@ extends CanvasModulate
 func _process(_delta):
 	var DNManager = DayAndNightCycleManager
 	var light = DNManager.day_progression*2
-	color.v = pingpong(light, 1.0)
+	color.v = pingpong(log(light), 1.0)
