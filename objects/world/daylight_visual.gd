ec9b1c34e2f925546176f8a9939da5b8b6403a31
Dalassos <143637128+Dalassos@users.noreply.github.com>
2025-03-01 00:11:54 +0000

    DN music and lights patch

--- objects/world/daylight_visual.gd
+++ objects/world/daylight_visual.gd
@@ -1,6 +1,29 @@
 extends CanvasModulate
+var DNManager = DayAndNightCycleManager
+var time : float
+var day_length : float = DNManager.DUSK_TIME - DNManager.DAWN_TIME
+var midday : float = DNManager.DAWN_TIME + (day_length)/2 
 
 func _process(_delta):
-	var DNManager = DayAndNightCycleManager
-	var light = DNManager.day_progression*2
-	color.v = pingpong(log(light), 1.0)
+	time = DNManager.day_progression
+	
+	color.v = get_brightness()
+	color.s = get_hue()
+	await get_tree().create_timer(3.0).timeout
+	
+	#var DNManager = DayAndNightCycleManager
+	#var light = DNManager.day_progression*2
+	#color.v = pingpong(log(light), 1.0)
+
+func get_brightness():
+	var bright :float = 1.5*(cos((time-midday)*TAU))+2.0*day_length
+	print(str(time) + "brightness is "+str(bright))
+	return max(min(bright,1.0), 0.0)
+
+	
+func get_hue():
+	var sine : float = sin((time-midday)*TAU)
+	#var hue : float = 3.0*(sine*sine)-2.5
+	var hue : float = 0.6-abs(get_brightness()-0.4)
+	print(str(time) + "hue is "+str(hue))
+	return max(min(hue,0.6),0.0)
