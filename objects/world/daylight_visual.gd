3fceaa7618281aaf89cfc5d3d141203cddcb2617
Dalassos <143637128+Dalassos@users.noreply.github.com>
2025-03-03 21:22:46 +0000

    remove debug code from daylight visuals

--- objects/world/daylight_visual.gd
+++ objects/world/daylight_visual.gd
@@ -11,13 +11,10 @@ func _process(_delta):
 	color.s = get_hue()
 	await get_tree().create_timer(3.0).timeout
 	
-	#var DNManager = DayAndNightCycleManager
-	#var light = DNManager.day_progression*2
-	#color.v = pingpong(log(light), 1.0)
 
 func get_brightness():
 	var bright :float = 1.5*(cos((time-midday)*TAU))+2.0*day_length
-	print(str(time) + "brightness is "+str(bright))
+	#print(str(time) + "brightness is "+str(bright))
 	return max(min(bright,1.0), 0.0)
 
 	
@@ -25,5 +22,5 @@ func get_hue():
 	var sine : float = sin((time-midday)*TAU)
 	#var hue : float = 3.0*(sine*sine)-2.5
 	var hue : float = 0.6-abs(get_brightness()-0.4)
-	print(str(time) + "hue is "+str(hue))
+	#print(str(time) + "hue is "+str(hue))
 	return max(min(hue,0.6),0.0)
